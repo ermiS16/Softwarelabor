04d0ed299be5e531bea8a6ae21878b127a9ff6b1
Eric Misfeld <eric_misf@gmx.de>
2018-11-18 13:19:10 +0100

    aufgabe2 implemented

--- aufgabe2.c
+++ aufgabe2.c
@@ -9,12 +9,29 @@
 #include "aufgabe2.h"
 
 char* extract(char *input, char *pattern){
-	char* position = input;
-	int indexInput=0;
-	int indexPattern=0;
-//	int patternLength = strlen(pattern);
-	printf("Stringlaenge: %d\n", strlen(input));
+	char *position = input;
+	char *temp;
+	int matching = 0;
 	printf("String: %s\n", input);
+
+	while(*input != '\0'){
+		if(*input == *pattern){
+			temp = input;
+			for(int i=0; i<sizeof(pattern); i++){
+				if(*temp == pattern[i]){
+					matching = 1;
+				}else{
+					matching = 0;
+					input = temp;
+				}
+				if(matching){
+					position = input+1;
+				}
+				input++;
+			}
+		}
+		input++;
+	}
 
 
 
